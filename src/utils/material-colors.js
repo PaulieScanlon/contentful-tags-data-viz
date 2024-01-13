b278e3ab63ef35e778507326308a65426a8fade5
Paul Scanlon <pauliescanlon@gmail.com>
2024-01-13 15:25:18 -0600

    feat: add dashboard

--- src/utils/material-colors.js
+++ src/utils/material-colors.js
@@ -11,12 +11,12 @@ const materialColors = {
   green: ['#388E3C', '#43A047', '#4CAF50', '#66BB6A', '#81C784', '#A5D6A7'],
   lightGreen: ['#7CB342', '#8BC34A', '#9CCC65', '#AED581', '#C5E1A5', '#DCEDC8'],
   lime: ['#AFB42B', '#C0CA33', '#CDDC39', '#D4E157', '#DCE775', '#E6EE9C'],
-  // yellow: ['#FBC02D', '#FDD835', '#FFEB3B', '#FFEE58', '#FFF176', '#FFF59D'],
+  yellow: ['#FBC02D', '#FDD835', '#FFEB3B', '#FFEE58', '#FFF176', '#FFF59D'],
   amber: ['#FFA000', '#FFB300', '#FFC107', '#FFCA28', '#FFD54F', '#FFE082'],
   orange: ['#F57C00', '#FB8C00', '#FF9800', '#FFA726', '#FFB74D', '#FFCC80'],
   deepOrange: ['#E64A19', '#F4511E', '#FF5722', '#FF7043', '#FF8A65', '#FFAB91'],
-  // blueGrey: ['#455A64', '#546E7A', '#607D8B', '#78909C', '#90A4AE', '#B0BEC5'],
-  // grey: ['#616161', '#757575', '#9E9E9E', '#BDBDBD', '#E0E0E0', '#EEEEEE'],
+  blueGrey: ['#455A64', '#546E7A', '#607D8B', '#78909C', '#90A4AE', '#B0BEC5'],
+  grey: ['#616161', '#757575', '#9E9E9E', '#BDBDBD', '#E0E0E0', '#EEEEEE'],
 };
 
 // Function to perform Fisher-Yates shuffle
