13f554d14bdf357ffeec731e86612470c03bc630
Paul Scanlon <pauliescanlon@gmail.com>
2024-01-13 16:41:05 -0600

    chore: remove netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import qwikdev from '@qwikdev/astro';
-import netlify from '@astrojs/netlify';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify(),
   integrations: [tailwind(), qwikdev()],
 });
