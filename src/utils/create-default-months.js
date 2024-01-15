3090511b5d6dbd58bce66340a15207bd3847eab9
Paul Scanlon <pauliescanlon@gmail.com>
2024-01-15 12:41:30 -0600

    refactor: move default months into createTimeSeries function

--- src/utils/create-default-months.js
+++
@@ -1,9 +0,0 @@
-export const createDefaultMonths = () => {
-  const monthsArray = Array.from({ length: 12 }, (_, index) => {
-    return new Date(2000, index, 1).toLocaleString('en-US', { month: 'short' });
-  }).map((month) => {
-    return { month, count: 0 };
-  });
-
-  return monthsArray;
-};
